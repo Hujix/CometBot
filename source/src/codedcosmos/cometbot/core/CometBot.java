9441d00980d52ea34f65d40f90ccfa397670fc95
codedcosmos <codedcosmos.mail@gmail.com>
2020-07-12 16:17:03 +1000

    2.1 quick patch

--- source/src/codedcosmos/cometbot/core/CometBot.java
+++ source/src/codedcosmos/cometbot/core/CometBot.java
@@ -35,7 +35,7 @@ import java.util.Random;
 public class CometBot {
 
 	// Cometbot
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "2.1";
 	public static String AVATAR_URL;
 	
 	// Guilds
